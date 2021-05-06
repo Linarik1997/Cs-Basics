f9050f0d37cae1ddb54b2a6544aec9f8ea01269d
Linar Khamitov <lk361353@gmail.com>
2021-05-06 23:18:12 +0300

    lesson3 homework

---
+++ Lesson3v1/Lesson3v1/Map.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace SeaBattle
+{
+    class Map
+    {
+        /// <summary>
+        /// Карта
+        /// </summary>
+        private char[,] map = new char[10, 10];
+        /// <summary>
+        /// Корабли на карте
+        /// </summary>
+        List<Ship> ships = new List<Ship>();
+        Random rnd = new Random();
+        /// <summary>
+        /// Не доступные клетки
+        /// </summary>
+        List<Point> blocked = new List<Point>();
+        /// <summary>
+        /// Кол-во попыток для расположения кораблей
+        /// </summary>
+        private int attempt = 0;
+
+        public int Attemp
+        {
+            get
+            {
+                return attempt;
+            }
+        }
+        
+        public void DrawMap(char empty, char placed) //Отрисовка карты
+        {
+            
+            foreach(var ship in ships)
+            {
+                for(int i = 0; i < ship.shipPosition.Count; i++)
+                {
+                    map[ship.shipPosition[i].X , ship.shipPosition[i].Y] = placed;
+                }
+            }
+            //Отладочный режим
+            //for (int i = 0; i < blocked.Count; i++)
+            //{
+            //    if(blocked[i].X>=0 && blocked[i].X < 10 && blocked[i].Y >= 0 && blocked[i].Y<10)
+            //    {
+            //        if(map[blocked[i].X,blocked[i].Y]!= placed)
+            //        {
+            //            map[blocked[i].X, blocked[i].Y] = closed;
+            //        }
+            //    }
+            //}
+
+            for(int i = 0; i < map.GetLength(0); i++)
+            {
+                for(int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == placed)
+                        Console.Write(placed + " ");
+                    //else if (map[i, j] == closed)
+                    //    Console.Write(closed + " ");
+                    else 
+                        Console.Write(empty + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+        private bool CanPlaceShip(Ship ship)//Проверка возможности расположения корабля
+        {
+            
+            bool intersect = true;
+            List<Point> check = Surround(ship); //Зона вокруг которой нельзя ставить корабль
+            //Проверка каждой точки корабля 
+            for (int i = 0; i < ship.shipPosition.Count ; i++)
+            {
+                //Если точка есть в списке не доустпных клеток, то возвращаем ложь
+                if (blocked.Contains(ship.shipPosition[i])) 
+                {
+                    intersect = false;
+                    return intersect;
+                }
+            }
+            //Если точки нет в списке не доступных - добавляем точки в список не доступных и возвращаем истину
+            for (int i = 0; i < check.Count; i++)
+            {
+                blocked.Add(check[i]);
+            }
+            return intersect;
+        }
+        public void PlaceShip(Ship ship)//расположение корабля на карте
+        {
+            bool isPlaced = false; //Успех - не успех
+            switch (ship.orientation)// в зависимости от ориентации корабля строим его точки
+            {
+                case (ShipOrientation.vertical):
+                    do
+                    {
+                        ship.x = rnd.Next(0, map.GetLength(0) );
+                        ship.y = rnd.Next(0, (map.GetLength(0)) - ship.length);
+                        for (int i = 0; i < ship.length; i++)
+                        {
+                            ship.shipPosition.Add(new Point(ship.x, ship.y + i));
+                        }
+                        if (CanPlaceShip(ship)) //Если успех - добавляем в общий список и признаку isPlaced присваиваем истину для выхода из цикла
+                        {
+                            ships.Add(ship);
+                            isPlaced = true;
+                        }
+                        else
+                        {
+                            ship.shipPosition.Clear(); //Если не успех - обнуляем список с точками корабля
+                            attempt++;
+                        }
+                    }
+                    while (!isPlaced);
+                    break;
+                case (ShipOrientation.hotizontal):
+                    do
+                    {
+                        ship.x = rnd.Next(0, (map.GetLength(0) ) - ship.length);
+                        ship.y = rnd.Next(0, map.GetLength(0)  );
+                        for (int i = 0; i < ship.length; i++)
+                        {
+                            ship.shipPosition.Add(new Point(ship.x + i, ship.y));
+                        }
+                        if (CanPlaceShip(ship))
+                        {
+                            ships.Add(ship);
+                            isPlaced = true;
+                        }
+                        else
+                        {
+                            ship.shipPosition.Clear();
+                            attempt++;
+                        }
+                    }
+                    while (!isPlaced);
+                    break;
+            }
+        }
+        private List<Point> Surround(Ship ship) //метод для получения точек вокруг корабля, чтобы понять, где нельзя будет расположить корабли
+        {
+            Point leftUp = new Point();
+            Point rightUp = new Point();
+            Point leftBot = new Point();
+            List<Point> rectangular = new List<Point>();
+            switch (ship.orientation)
+            {
+                case (ShipOrientation.vertical):
+                    leftUp.X = ship.shipPosition[0].X - 1;
+                    leftUp.Y = ship.shipPosition[0].Y - 1;
+                    rightUp.X = ship.shipPosition[0].X + 1;
+                    rightUp.Y = ship.shipPosition[0].Y - 1;
+                    leftBot.X = ship.shipPosition[ship.shipPosition.Count - 1].X - 1;
+                    leftBot.Y = ship.shipPosition[ship.shipPosition.Count - 1].Y + 1;
+                    for(int i = leftUp.X; i <= rightUp.X; i++)
+                    {
+                        for(int j = leftUp.Y; j <= leftBot.Y; j++)
+                        {
+                            rectangular.Add(new Point(i, j));
+                        }
+                    }
+                    return rectangular;
+                case (ShipOrientation.hotizontal):
+                    leftUp.X = ship.shipPosition[0].X - 1;
+                    leftUp.Y = ship.shipPosition[0].Y - 1;
+                    rightUp.X = ship.shipPosition[ship.shipPosition.Count - 1].X + 1;
+                    rightUp.Y = ship.shipPosition[ship.shipPosition.Count - 1].Y - 1;
+                    leftBot.X = ship.shipPosition[0].X - 1;
+                    leftBot.Y = ship.shipPosition[0].Y + 1;
+                    for (int i = leftUp.X; i <= rightUp.X; i++)
+                    {
+                        for (int j = leftUp.Y; j <= leftBot.Y; j++)
+                        {
+                            rectangular.Add(new Point(i, j));
+                        }
+                    }
+                    return rectangular;
+                default:
+                    return rectangular;
+            }
+        }
+    }
+}
---
+++ Lesson3v1/Lesson3v1/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SeaBattle
+{
+    
+    class Program
+    {
+        public static string GenerateMail(string name)
+        {
+            Random rnd = new Random();
+            Regex eMail = new Regex(@"[A-Za-z]+[\.A-Za-z0-9_-]*[A-Za-z0-9]+@[A-Za-z]+\.[A-Za-z]+");
+            string[] domen = { "@gmail.com", "@outlook.com", "@mail.ru", "@yandex.ru" };
+            string mail = $"{name}{rnd.Next(0, 100)}{domen[rnd.Next(0, domen.Length)]}";
+            if (eMail.IsMatch(mail))
+            {
+                return mail;
+            }
+            else
+            {
+                throw new Exception("Doesn't match RegEx");
+            }
+        }
+        public static char[] Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            char[] reversed = new char[letters.Length];
+            for(int i = 0; i < reversed.Length; i++)
+            {
+                reversed[i] = letters[reversed.Length - i  - 1];
+            }
+            return reversed;
+        }
+        
+        public static void Diagonal(int [,] array, int offset)
+        {
+            if (array.GetLength(0) != array.GetLength(1))
+            {
+                Console.WriteLine("Измерения массивы должны быть соразмерны");
+            }
+            else
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (i == j || j == array.GetLength(1) - (i + 1))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        else
+                        {
+                            Console.ResetColor();
+                        }
+                        array[i, j] = rnd.Next(0, 100);
+                        if (array[i, j].ToString().Length < 2)
+                        {
+                            Console.Write(array[i, j] + "  ");
+                        }
+                        else
+                            Console.Write(array[i, j] + " ");
+                    }
+                    Console.WriteLine("");
+                }
+                Console.ResetColor();
+                Console.WriteLine("");
+
+                for (int i = 0; i < array.GetLength(1); i++)
+                {
+                    Console.SetCursorPosition(i * offset, Console.CursorTop);
+                    Console.Write(array[i, i]);
+                    Console.SetCursorPosition(array.GetLength(1) * offset - ((i + 1) * offset), Console.CursorTop);
+                    Console.Write(array[i, array.GetLength(1) - (i + 1)]);
+                    Console.WriteLine("");
+                }
+            }
+        }
+        public static void Task1()
+        {
+            Console.WriteLine("Вывод диагонали квадртаного массива");
+            Console.ReadLine();
+            Random rnd = new Random();
+            int[,] array = new int[10, 10];
+            Diagonal(array, 3);
+            Console.ReadLine();
+        }
+        public static void Task2()
+        {
+            Console.WriteLine("Введите набор текста, а программа напишет его в обратном направлении");
+            var input = Console.ReadLine();
+            var word = Reverse(input);
+            foreach (var letter in word)
+            {
+                Console.Write(letter);
+            }
+            Console.ReadLine();
+        }
+        public static void Task3()
+        {
+            Console.WriteLine("Массив имен и электронных адрессов");
+            string[] names = { "Linar", "Ivan", "Nikolai", "Alena", "Katya" };
+            string[,] mailBook = new string[5, 2];
+            for (int i = 0; i < mailBook.GetLength(0); i++)
+            {
+                mailBook[i, 0] = names[i];
+                mailBook[i, 1] = GenerateMail(names[i]);
+            }
+            for (int i = 0; i < mailBook.GetLength(0); i++)
+            {
+                Console.WriteLine($"{mailBook[i, 0]}: {mailBook[i, 1]}");
+            }
+        }
+        public static void Task4()
+        {
+            Console.WriteLine("Случайное расположение кораблей в игре Морской Бой");
+            Console.ReadLine();
+            var now = DateTime.Now;
+            Map map = new Map();
+            List<Ship> ships = new List<Ship>();
+            ships = Ship.defShips();
+            foreach (var ship in ships)
+            {
+                map.PlaceShip(ship);
+            }
+            map.DrawMap('0', 'X');
+            var end = DateTime.Now;
+            Console.WriteLine($"Время выполнения программы:{end - now}; Количество попыток:{map.Attemp}");
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Здравствуйте! Вас приветствует мастер проверки домашнего задания 3\nВведите номер задания для проверки: \n1. " +
+                "Вывод диагонали\n2. Вывод текста в обратном направлении\n3. Массив имен и электронных адрессов\n" +
+                "4. Случайное расположение кораблей в игре Морской Бой\nДля выхода нажмите 0");
+                var choice = Console.ReadLine();
+                if (int.TryParse(choice, out int res))
+                {
+                    switch (res)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Task1();
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Task2();
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        case 3:
+                            Console.Clear();
+                            Task3();
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Console.Clear();
+                            Task4();
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        case 0:
+                            return;
+                        default:
+                            Console.WriteLine("Упс, кажется вы промахнулись");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не число!");
+                }
+            }
+        }
+    }
+}
